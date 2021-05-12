4089213dc4f1ea229e0accc3a6e76d6a2eb5dbcc
matteofabris <teo.fbr92@gmail.com>
2021-05-12 23:27:00 +0200

    di: remove unuseful code

--- app/src/main/java/com/example/wordsmemory/vocabulary/category/CategoryFragment.kt
+++ app/src/main/java/com/example/wordsmemory/vocabulary/category/CategoryFragment.kt
@@ -7,8 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.wordsmemory.R
-import com.example.wordsmemory.database.VocabularyDao
-import com.example.wordsmemory.database.VocabularyDatabase
 import com.example.wordsmemory.databinding.CategoryFragmentBinding
 import com.example.wordsmemory.vocabulary.words.VocabularyWordsFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,7 +16,6 @@ import kotlinx.coroutines.InternalCoroutinesApi
 @AndroidEntryPoint
 class CategoryFragment : Fragment() {
 
-    private lateinit var _dbDao: VocabularyDao
     private val _viewModel: CategoryViewModel by viewModels()
     private lateinit var _binding: CategoryFragmentBinding
 
@@ -28,17 +25,11 @@ class CategoryFragment : Fragment() {
     ): View {
         _binding = CategoryFragmentBinding.inflate(inflater)
         _binding.categoryViewmodel = _viewModel
-        _dbDao = getDbDao()
 
         childFragmentManager.beginTransaction()
             .replace(R.id.category_vocabulary_container, VocabularyWordsFragment())
             .commit()
 
         return _binding.root
-    }
-
-    private fun getDbDao(): VocabularyDao {
-        val application = requireNotNull(this.activity).application
-        return VocabularyDatabase.getInstance(application).vocabularyDao()
     }
 }
