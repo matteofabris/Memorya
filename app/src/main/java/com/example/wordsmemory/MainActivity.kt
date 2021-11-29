990827e8f3347736f60df359590f3d0c086aa8dc
matteofabris <teo.fbr92@gmail.com>
2021-11-29 20:01:21 +0100

    splashScreen: implementation

--- app/src/main/java/com/example/wordsmemory/MainActivity.kt
+++ app/src/main/java/com/example/wordsmemory/MainActivity.kt
@@ -6,20 +6,17 @@ import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.example.wordsmemory.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlin.math.sqrt
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var _binding: ActivityMainBinding
-
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
         requestedOrientation = if (isTablet()) {
             Log.i(Constants.packageName, "Tablet mode")
