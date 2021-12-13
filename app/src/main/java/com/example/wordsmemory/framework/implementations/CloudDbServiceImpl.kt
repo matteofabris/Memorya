50b5c6d587d625e483c0c61aaa24ccc050444aed
matteofabris <teo.fbr92@gmail.com>
2021-12-13 19:51:28 +0100

    workRequestBuilder: created static builder

--- app/src/main/java/com/example/wordsmemory/framework/implementations/CloudDbServiceImpl.kt
+++ app/src/main/java/com/example/wordsmemory/framework/implementations/CloudDbServiceImpl.kt
@@ -1,17 +1,18 @@
 package com.example.wordsmemory.framework.implementations
 
-import androidx.work.*
+import androidx.work.WorkManager
 import com.example.wordsmemory.Constants
 import com.example.wordsmemory.data.interfaces.CloudDbService
 import com.example.wordsmemory.domain.Constants.CloudDbObjectType
 import com.example.wordsmemory.domain.Constants.CloudDbObjectType.*
-import com.example.wordsmemory.framework.worker.CloudDbSyncWorker
-import java.util.concurrent.TimeUnit
+import com.example.wordsmemory.framework.worker.WorkRequestBuilder
 import javax.inject.Inject
 
 class CloudDbServiceImpl @Inject constructor(private val _workManager: WorkManager) :
     CloudDbService {
-    override fun fetchCloudDb() = createWorkRequest(Constants.CloudDbSyncWorkType.Fetch)
+    override fun fetchCloudDb() {
+        _workManager.enqueue(WorkRequestBuilder.build(Constants.CloudDbSyncWorkType.Fetch))
+    }
 
     override fun add(type: CloudDbObjectType, id: Int) {
         val workType = when (type) {
@@ -20,7 +21,7 @@ class CloudDbServiceImpl @Inject constructor(private val _workManager: WorkManag
             Category -> Constants.CloudDbSyncWorkType.InsertCategory
         }
 
-        createWorkRequest(workType, id)
+        _workManager.enqueue(WorkRequestBuilder.build(workType, id))
     }
 
     override fun remove(type: CloudDbObjectType, id: Int) {
@@ -30,33 +31,6 @@ class CloudDbServiceImpl @Inject constructor(private val _workManager: WorkManag
             User -> return
         }
 
-        createWorkRequest(workType, id)
-    }
-
-    private fun createWorkRequest(workType: Constants.CloudDbSyncWorkType, id: Int = -1) {
-        val workData = getWorkData(workType, id)
-        val workRequest = OneTimeWorkRequestBuilder<CloudDbSyncWorker>()
-            .setBackoffCriteria(
-                BackoffPolicy.EXPONENTIAL,
-                OneTimeWorkRequest.MIN_BACKOFF_MILLIS,
-                TimeUnit.MILLISECONDS
-            ).setInputData(workData).build()
-        _workManager.enqueue(workRequest)
-    }
-
-    private fun getWorkData(workType: Constants.CloudDbSyncWorkType, id: Int = -1): Data {
-        return when (workType) {
-            Constants.CloudDbSyncWorkType.InsertUser, Constants.CloudDbSyncWorkType.Fetch -> {
-                workDataOf(
-                    Constants.WORK_TYPE to workType.name
-                )
-            }
-            else -> {
-                workDataOf(
-                    Constants.WORK_TYPE to workType.name,
-                    Constants.ITEM_ID to id
-                )
-            }
-        }
+        _workManager.enqueue(WorkRequestBuilder.build(workType, id))
     }
 }
